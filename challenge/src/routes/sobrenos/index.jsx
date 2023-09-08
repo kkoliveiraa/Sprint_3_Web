4aa14d51678acb4ebcb9aa310b716ba0a1da83ed
EduBatageli <edumbat@gmail.com>
2023-09-08 13:16:42 -0300

    img 2 e 3 no jsx

--- challenge/src/routes/sobrenos/index.jsx
+++ challenge/src/routes/sobrenos/index.jsx
@@ -1,6 +1,7 @@
 import motorista from "../../assets/motorista.png"
 import rafa from "../../assets/Rafael Minoro itokazo.png"
-
+import edu from "../../assets/Eduardo Moura Batageli.png"
+import thiago from "../../assets/Thiago Gil Camargo.png"
 
 export default function Sobrenos(){
 
@@ -8,6 +9,7 @@ export default function Sobrenos(){
     return(
         <main>
             <img id="motorista"src={rafa} alt="motorista de caminhão dirigindo" />
+            <img id="motorista"src={edu} alt="motorista de caminhão dirigindo" />
             <h1>tetseee</h1>
         </main>
     )
