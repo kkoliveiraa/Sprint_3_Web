ed0e8114bee3584bb44941262e090c1ac4298125
EduBatageli <edumbat@gmail.com>
2023-09-08 13:14:26 -0300

    colocando imagem 1 np jsx

--- challenge/src/routes/sobrenos/index.jsx
+++ challenge/src/routes/sobrenos/index.jsx
@@ -1,11 +1,13 @@
 import motorista from "../../assets/motorista.png"
+import rafa from "../../assets/Rafael Minoro itokazo.png"
+
 
 export default function Sobrenos(){
 
     
     return(
         <main>
-            <img id="motorista"src={motorista} alt="motorista de caminhão dirigindo" />
+            <img id="motorista"src={rafa} alt="motorista de caminhão dirigindo" />
             <h1>tetseee</h1>
         </main>
     )
