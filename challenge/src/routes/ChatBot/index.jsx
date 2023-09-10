fe22e49f2207a75756393e0758254c6b114c0620
kkoliveiraa <RM550815@fiap.com.br>
2023-09-10 15:59:08 -0300

    Revert "ajustando nomenclatura das assets"
    This reverts commit 025904f24e8d830181b0ae89f1edf90be44152ec.

--- challenge/src/routes/ChatBot/index.jsx
+++ challenge/src/routes/ChatBot/index.jsx
@@ -1,5 +1,5 @@
-import portinho from "../../assets/portinho.png"
-import iconePortinho from "../../assets/icone-portinho.png"
+import portinho from "../../assets/Portinho.png"
+import iconePortinho from "../../assets/iconePortinho.png"
 export default function ChatBot(){
 
     return(
