35c7db24e5500ab3c01179492d70c27e11f23b6d
Rminoro <rminoro@outlook.com>
2023-09-12 00:54:30 -0300

    Footer

--- challenge/src/components/rodape.jsx
+++ challenge/src/components/rodape.jsx
@@ -5,11 +5,11 @@ import "../components/styleFooter.css";
 export default function Rodape(){
 
     return(
-        <footer className='rodape'>
-            <p>Entre em contato conosco</p>
-            <p>Quer saber mais sobre os Devs por trás do projeto? Clique no botão abaixo e confira</p>
+        <footer id="footerStyle">
+            <p className="conteudoFooter">Entre em contato conosco</p>
+            <p className="conteudoFooter">Quer saber mais sobre os Devs por trás do projeto? Clique no botão abaixo e confira</p>
             <Link to ="/sobrenos">
-                <button> Sobre Nós</button>
+                <button className='BotaoSobrenos'> Sobre Nós</button>
             </Link>
            
             <p>@Grupo ntc<span>|</span>grupontc1@gmail.com<span>|</span> (11)98716-6289</p>
@@ -18,4 +18,3 @@ export default function Rodape(){
         
     )
 }
-
